c0254aa8dbbb7ba2809656d654ff0ad4d798fd59
jeremite <543084991@qq.com>
2020-11-02 08:53:01 -0800

    using click package

--- grouping/cli.py
+++ grouping/cli.py
@@ -3,6 +3,7 @@ import pkg_resources
 import sys
 #from config import configs
 import argparse
+import click
 import json
 
 def change_config(bkt,pre):
@@ -13,15 +14,17 @@ def change_config(bkt,pre):
     with open('config.json', 'w') as outfile:
         json.dump(data, outfile)
 
+@click.command()
+@click.option('-b', '--bucket',prompt="bucket name",default='va-vdas-workspace-prod')
+@click.option('-p', '--prefix',prompt="path after bucket")
+def main(bucket,prefix):
+    #parser = argparse.ArgumentParser(description='get s3 info.')
+    #parser.add_argument('-b', '--bucket',help='input bucket name.')
+    #parser.add_argument('-p', '--prefix',help='input prefix.')
 
-def main():
-    parser = argparse.ArgumentParser(description='get s3 info.')
-    parser.add_argument('-b', '--bucket',help='input bucket name.')
-    parser.add_argument('-p', '--prefix',help='input prefix.')
-
-    res = vars(parser.parse_args())
-    print('check:',(res['bucket'],res['prefix']))
-    change_config(res['bucket'],res['prefix'])
+    #res = vars(parser.parse_args())
+    print('check:',(bucket,prefix))
+    change_config(bucket,prefix)
 
     #os.chdir(os.path.join(pkg_resources.get_distribution("grouping").location,"grouping"))
     os.system('docker-compose up')# --build')
